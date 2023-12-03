e0e14340a0d9165f909d87008747d1b268fc975b
Nerfensss <130859641+Nerfensss@users.noreply.github.com>
2023-12-03 14:54:57 +0200

    Add files via upload

---
+++ CodeTest.cs
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m, a, b, k, c;
+
+    cout << "Введіть кількість людей (n): ";
+    cin >> n;
+
+    cout << "Введіть кількість поїздок на кожну людину (m): ";
+    cin >> m;
+
+    cout << "Введіть вартість квитка на одну поїздку (a): ";
+    cin >> a;
+
+    cout << "Введіть вартість безлімітного квитка на одну людину (b): ";
+    cin >> b;
+
+    cout << "Введіть кількість людей для групового квитка (k): ";
+    cin >> k;
+
+    cout << "Введіть вартість групового безлімітного квитка (c): ";
+    cin >> c;
+
+
+    int totalCost = 0;
+    int groupCount = n / k; 
+    int remainingPeople = n % k; 
+
+    totalCost += groupCount * min(c, k * b);
+    totalCost += remainingPeople * min(b, a * m);
+
+    cout << "Мінімальна сумарна вартість квитків: " << totalCost << " гривень" << endl;
+
+    return 0;
+}
+
